be7bc3e4854a3cbc8f5abf94a565d1f8740ecd6b
Your Name <death5819@gmail.com>
2025-01-29 04:31:35 -0500

    1/29/25 4:30

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 //=======================================================================================/
-btnsOpenModal.forEach(btnOpenModal => btnOpenModal.addEventListener("click", openModal));
+btnsOpenModal.forEach(btnOpenModal =>
+  btnOpenModal.addEventListener('click', openModal)
+);
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 //=======================================================================================//
@@ -40,20 +42,21 @@ console.log(document.head);
 console.log(document.body);
 
 const header = document.querySelector('.header');
-const allSections = document.querySelectorAll(".section");
+const allSections = document.querySelectorAll('.section');
 document.querySelectorAll('.section');
 
-document.getElementById("section--1");
+document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
-console.log(document.getElementsByClassName("btn"));
+console.log(document.getElementsByClassName('btn'));
 
 const message = document.createElement('div');
-message.classList.add('cookie-message')
+message.classList.add('cookie-message');
 // message.textContent = "We use cookies for improved funcionality and analytics.";
-message.innerHTML = "We use cookies for improved funcionality plus more. <button class='btn btn--close-cookie'>Got it!</button>";
-message.style.color = "black";
+message.innerHTML =
+  "We use cookies for improved funcionality plus more. <button class='btn btn--close-cookie'>Got it!</button>";
+message.style.color = 'black';
 
 // header.prepend(message);
 header.append(message);
@@ -68,14 +71,12 @@ const btnClose = document.querySelector('.btn--close-cookie');
 btnClose.addEventListener('click', function () {
   message.remove();
 });
-message.style.backgroundColor = "#e38c0b";
-message.style.padding = "5px";
-message.style.width = "110%";
-const testImg = document.querySelector("#features__img");
+message.style.backgroundColor = '#e38c0b';
+message.style.padding = '5px';
+message.style.width = '110%';
+const testImg = document.querySelector('#features__img');
 console.log(testImg.alt);
 console.log(message);
-
-
 
 // //======================================================//
 // const imgDiv = document.createElement("div");
@@ -85,3 +86,39 @@ console.log(message);
 // header.appendChild(imgDiv);
 // //=====================================================//
 // console.log(imgDiv);
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  //Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListen: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 2000);
